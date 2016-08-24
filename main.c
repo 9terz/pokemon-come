2244f411c353734597469ec7e4c6c0397a76d1a6
9terz <9terz666@gmail.com>
2016-08-24 11:05:43 +0700

    git commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void sum(int,int);
+int main()
+{
+  printf('sawaddee kub')
+  sum(1,10);
+}
+
+void (int from, int to)
+{
+  int i;
+  for(i = from; i <=to; i++)
+  {
+    sum += 1;
+  }
+
+}
